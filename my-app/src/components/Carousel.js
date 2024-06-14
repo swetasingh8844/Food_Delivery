0a74d5cc3b67217336cd52280938d7fd09a5e7ae
swetasingh8844 <swetasingh8844@gmail.com>
2024-06-15 00:42:47 +0530

    done

---
+++ my-app/src/components/Carousel.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function Carousel() {
+  return (
+    <div>
+        <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="https://th.bing.com/th/id/R.e8074f5da68c54a43ac40a2b081fa91d?rik=y6KLQo0vHpQa8Q&riu=http%3a%2f%2fimages.unsplash.com%2fphoto-1604409969824-6cdc3f32d682%3fixlib%3drb-1.2.1%26q%3d80%26fm%3djpg%26crop%3dentropy%26cs%3dtinysrgb%26w%3d1080%26fit%3dmax&ehk=xLsRLM9Op7fVCI%2faJRsLgIEF%2fBrQwUInsNmuoMdOuOs%3d&risl=&pid=ImgRaw&r=0" className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="https://th.bing.com/th/id/R.e8074f5da68c54a43ac40a2b081fa91d?rik=y6KLQo0vHpQa8Q&riu=http%3a%2f%2fimages.unsplash.com%2fphoto-1604409969824-6cdc3f32d682%3fixlib%3drb-1.2.1%26q%3d80%26fm%3djpg%26crop%3dentropy%26cs%3dtinysrgb%26w%3d1080%26fit%3dmax&ehk=xLsRLM9Op7fVCI%2faJRsLgIEF%2fBrQwUInsNmuoMdOuOs%3d&risl=&pid=ImgRaw&r=0" className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="https://th.bing.com/th/id/R.e8074f5da68c54a43ac40a2b081fa91d?rik=y6KLQo0vHpQa8Q&riu=http%3a%2f%2fimages.unsplash.com%2fphoto-1604409969824-6cdc3f32d682%3fixlib%3drb-1.2.1%26q%3d80%26fm%3djpg%26crop%3dentropy%26cs%3dtinysrgb%26w%3d1080%26fit%3dmax&ehk=xLsRLM9Op7fVCI%2faJRsLgIEF%2fBrQwUInsNmuoMdOuOs%3d&risl=&pid=ImgRaw&r=0" className="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+    </div>
+  )
+}
--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 export default function Navbar() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+      <nav className="navbar navbar-expand-lg navbar-success bg-success">
   <div className="container-fluid">
     <Link className="navbar-brand fs-1 fst-italic" to="/">GoFood</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
