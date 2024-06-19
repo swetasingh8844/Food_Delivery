7dd35064f08e2de6e274108529bda55cffe30313
swetasingh8844 <swetasingh8844@gmail.com>
2024-06-19 22:50:34 +0530

    done

--- my-app/backend/db.js
+++ my-app/backend/db.js
@@ -1,19 +1,22 @@
 const mongoose = require('mongoose');
-const mongoURI='mongodb+srv://swetasingh8844:P52VdWcoIhyw0X1u@cluster0.kaqutvm.mongodb.net/gofoodmern?retryWrites=true&w=majority&appName=Cluster0'
+//  const mongoURI='mongodb+srv://swetasingh8844:P52VdWcoIhyw0X1u@cluster0.kaqutvm.mongodb.net/gofoodmern?retryWrites=true&w=majority&appName=Cluster0'
 // mongodb+srv://<username>:<password>@cluster0.keitd5c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+
+// const mongoose = require('mongoose');
+
 const mongoDB = async () => {
-  
-    await mongoose.connect(mongoURI, { useNewUrlParser: true}, async(err,result)=>{
-        if(err) {
-            console.error('Failed to connect to MongoDB', err);
-        }
-    
-    else{
-        console.log('Connected to MongoDB');
-    }
-});
-  
-}
-module.exports = mongoDB();
+  const connectionString = 'mongodb+srv://swetasingh8844:P52VdWcoIhyw0X1u@cluster0.kaqutvm.mongodb.net/gofoodmern?retryWrites=true&w=majority&appName=Cluster0'; // replace with your actual connection string
 
+  try {
+    await mongoose.connect(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Successfully connected to MongoDB');
+  } catch (err) {
+    console.error('Error connecting to MongoDB', err);
+  }
+};
 
+module.exports = mongoDB;
